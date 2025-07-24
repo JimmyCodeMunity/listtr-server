9f2e3256dad6bc2896535ae73143a10fd703a1cd
Digitalqatalyst <dq.demo.a@digitalqatalyst.com>
2025-07-24 04:44:27 -0700

    server

--- index.js
+++ index.js
@@ -10,15 +10,20 @@ require("dotenv").config({
 
 app.use(
   cors({
-    origin: process.env.CLIENT_URL,//allowed front end url
-    credentials: true,//allow cookies
-    methods: ["GET", "POST", "PUT", "DELETE"],//allowed methods
-    allowedHeaders: ["Content-Type", "Authorization"],//allowed headers
+    origin: process.env.CLIENT_URL, //allowed front end url
+    credentials: true, //allow cookies
+    methods: ["GET", "POST", "PUT", "DELETE"], //allowed methods
+    allowedHeaders: ["Content-Type", "Authorization"], //allowed headers
   })
 );
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+
+// log in browser
+app.get("/", (req, res) => {
+  res.send("Listtr server up!");
+});
 
 const PORT = process.env.PORT || 3000;
 
